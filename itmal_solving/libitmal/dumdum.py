52091d788a548e4dd87b454a8e6ca6c80d97973d
Asbjørn T. Gyring <assi90@gmail.com>
2019-02-24 17:42:38 +0100

    Metics is almost done. The last task, Qe requires us to redo the steps learned through the previous ones, on a new dataset. This mean downloading it, preparing the data with selected features etc, cleaning the data if values for the features are missing, training the model, and meassuring its accuracy and other performance metrics.

--- itmal_solving/libitmal/dumdum.py
+++ itmal_solving/libitmal/dumdum.py
@@ -1,10 +1,10 @@
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, ClassifierMixin
 import numpy as np
 # from sklearn.model_selection import cross_val_score
 # from sklearn.model_selection import cross_val_predict
 # from sklearn.metrics import confusion_matrix
 
-class DummyClassifier(BaseEstimator):
+class DummyClassifier(BaseEstimator, ClassifierMixin):
     def fit(self, X, y=None):
         pass
     
